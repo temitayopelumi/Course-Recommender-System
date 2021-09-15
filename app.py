163062a66b027c8e171493e67d9f3f7d6f4760d2
temitayopelumi <olalekantemitayo7@gmail.com>
2021-09-15 18:53:39 +0100

    deployment

--- app.py
+++ app.py
@@ -5,53 +5,53 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/CRS'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/CRS'
+# db = SQLAlchemy(app)
 
 
 ###Models####
-class Course(db.Model):
-    __tablename__ = "courses"
-    id = db.Column(db.Integer, primary_key=True)
-    course_code = db.Column(db.String(10))
-    course_title = db.Column(db.String(100))
-    level = db.Column(db.String(100))
-    option = db.Column(db.String(100))
-    semester = db.Column(db.String(100))
-    unit = db.Column(db.Integer)
+# class Course(db.Model):
+#     __tablename__ = "courses"
+#     id = db.Column(db.Integer, primary_key=True)
+#     course_code = db.Column(db.String(10))
+#     course_title = db.Column(db.String(100))
+#     level = db.Column(db.String(100))
+#     option = db.Column(db.String(100))
+#     semester = db.Column(db.String(100))
+#     unit = db.Column(db.Integer)
 
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
+#     def create(self):
+#         db.session.add(self)
+#         db.session.commit()
+#         return self
 
-    def __init__(self, course_code, course_title, level, option,semester, unit):
-        self.course_code = course_code
-        self.course_title = course_title
-        self.level = level
-        self.option = option
-        self.semester = semester
-        self.unit = unit
+#     def __init__(self, course_code, course_title, level, option,semester, unit):
+#         self.course_code = course_code
+#         self.course_title = course_title
+#         self.level = level
+#         self.option = option
+#         self.semester = semester
+#         self.unit = unit
 
-    def __repr__(self):
-        return '' % self.id
-
-
-db.create_all()
+#     def __repr__(self):
+#         return '' % self.id
 
 
-class CourseSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Course
-        sqla_session = db.session
+# db.create_all()
 
-    id = fields.Number(dump_only=True)
-    course_code = fields.String(required=True)
-    course_title = fields.String(required=True)
-    level = fields.String(required=True)
-    option = fields.String(required=True)
-    semester = fields.String(required=True)
-    unit = fields.Number(required=True)
+
+# class CourseSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = Course
+#         sqla_session = db.session
+
+#     id = fields.Number(dump_only=True)
+#     course_code = fields.String(required=True)
+#     course_title = fields.String(required=True)
+#     level = fields.String(required=True)
+#     option = fields.String(required=True)
+#     semester = fields.String(required=True)
+#     unit = fields.Number(required=True)
 
 
 @app.route('/')
